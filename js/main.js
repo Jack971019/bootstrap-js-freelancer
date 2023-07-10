2ce0fdb83853fd02701569e3027d7825e27c0e22
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-10 12:52:18 +0200

    final cost calculated

--- js/main.js
+++ js/main.js
@@ -4,35 +4,37 @@ let sendButton = document.getElementById("send_button");
 
 
 
-    sendButton.addEventListener("click", function (event) {
-        event.preventDefault();
+sendButton.addEventListener("click", function (event) {
+    event.preventDefault();
 
-    
 
-    document.getElementById("formGroupExampleInput").value
 
-    let hoursrequested = parseInt(document.getElementById("hours_requested").value);
-    let typeofwork = document.getElementById("type_of_work").value;
-    let discountcode = document.getElementById("discount_code").value;
+
+
+    let hoursrequested = parseInt(document.getElementById("formGroupExampleInput2").value);
+    let typeofwork = parseInt(document.getElementById("type_of_work").value);
+    let discountcode = document.getElementById("formGroupExampleInput").value;
 
     let finalPrice = 0
 
     if (typeofwork == "1") {
         finalPrice = 20.5 * hoursrequested;
-    }  else if (typeofwork == "2") {
+    } else if (typeofwork == "2") {
         finalPrice = 15.3 * hoursrequested;
-    }  else if (typeofwork == "3") {
+    } else if (typeofwork == "3") {
         finalPrice = 33.6 * hoursrequested;
     }
 
-    console.log(finalPrice);
 
+    let discountpresent = false
     for (let i = 0; i < arrayDiscountCodes.length; i++) {
         if (arrayDiscountCodes[i] == discountcode) {
-            finalPrice = finalPrice - finalPrice * 25 / 100;
+            discountpresent = true;
         }
     }
-
+    if (discountpresent) {
+        finalPrice = finalPrice * 0.75;
+    }
     finalPrice = finalPrice.toFixed(2);
 
     document.getElementById("final_price").innerHTML = "Il prezzo è di: " + finalPrice + "$";
