1e3313528b1fac64d0ae4a3f38c59970afe6f204
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-10 10:39:32 +0200

    value taken from html

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+let arrayDiscountCodes = ["YHDNU32", "JANJC63" ,"PWKCN25", "SJDPO96", "POCIE24"];
+
+
+
+
+document.getElementById("send_button").addEventListener("click", function() {
+    alert("Hello World!");
+  });
+
+document.getElementById("formGroupExampleInput").value
+
+let hoursrequested = parseInt(document.getElementById("hours_requested").value);
+let typeofwork = document.getElementById("type_of_work").value;
+let discountcode = document.getElementById("discount_code").value;
